b80ca397dac26ece6adaabff3f4e95fac7b2b17b
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-28 22:25:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,60 @@
-// your code here
-document.addEventListener("DOMContentLoaded", function(e) {
-  debugger
+// // your code here
+// document.addEventListener("DOMContentLoaded", function(e) {
+//   debugger
 
-  const newTaskForm = document.getElementById("create-task-form")
-  const newTaskDescription = document.getElementById("new-task-description")
+//   const newTaskForm = document.getElementById("create-task-form")
+//   const newTaskDescription = document.getElementById("new-task-description")
 
+//   const newTaskUl = document.getElementById("tasks");
+
+//   newTaskForm.addEventListener("submit", createNewTask);
+// });
+
+// const createNewTask = e => {
+//   e.preventDefault();
+
+//   const newTaskDescription = document.getElementById("new-task-description");
+//   const newTask = document.createElement("li");
+//   newTask.innerText = newTaskDescription.value;
+
+//   appendNewTask(newTask);
+//   e.target.reset();
+// }
+
+// const appendNewTask = task => {
+//   document.getElementById("tasks").appendChild(task);
+// };
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  //grab all the necessary DOM elements
+
+  //form and relevant input fields
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  //ul where new tasks will live on the DOM
   const newTaskUl = document.getElementById("tasks");
 
+  //attach event listeners
   newTaskForm.addEventListener("submit", createNewTask);
 });
 
-const createNewTask = e => {
-  e.preventDefault();
-
+const createNewTask = event => {
+  event.preventDefault();
+  //stop form from trying to submit
   const newTaskDescription = document.getElementById("new-task-description");
   const newTask = document.createElement("li");
   newTask.innerText = newTaskDescription.value;
 
   appendNewTask(newTask);
-  e.target.reset();
-}
+  event.target.reset();
+};
 
 const appendNewTask = task => {
   document.getElementById("tasks").appendChild(task);
 };
-
-
 
